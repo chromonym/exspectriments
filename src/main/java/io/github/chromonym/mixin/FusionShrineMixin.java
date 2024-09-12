64e1202ca45318d20feb9f38c81a842b1d900175
Cyan <44630816+chromonym@users.noreply.github.com>
2024-09-12 11:45:03 +0930

    liquid recipes!
    yes i know i've just hardcoded it. shut

--- src/main/java/io/github/chromonym/mixin/FusionShrineMixin.java
+++ src/main/java/io/github/chromonym/mixin/FusionShrineMixin.java
@@ -8,12 +8,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import de.dafuqs.spectrum.blocks.fusion_shrine.FusionShrineBlock;
 import de.dafuqs.spectrum.blocks.fusion_shrine.FusionShrineBlockEntity;
 import de.dafuqs.spectrum.recipe.fusion_shrine.FusionShrineRecipe;
+import de.dafuqs.spectrum.registries.SpectrumFluids;
 import io.github.chromonym.ExspFluids;
-import io.github.chromonym.Exspectriments;
 
 import static net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants.BUCKET;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.minecraft.block.BlockState;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -21,11 +22,25 @@ import net.minecraft.world.World;
 public class FusionShrineMixin {
     @Inject(at = @At("TAIL"), method = "craft")
     private static void afterCraft(World world, BlockPos blockPos, FusionShrineBlockEntity fusionShrineBlockEntity, FusionShrineRecipe recipe, CallbackInfo info) {
-        Exspectriments.LOGGER.info(recipe.getId().toString());
-        if (recipe.getId().toString().equals("exspectriments:fusion_shrine/crystal_to_topaz")) {
-            fusionShrineBlockEntity.fluidStorage.variant = FluidVariant.of(ExspFluids.LIQUID_TOPAZ);
-            fusionShrineBlockEntity.fluidStorage.amount = BUCKET;
-            world.setBlockState(blockPos, (BlockState)world.getBlockState(blockPos).with(FusionShrineBlock.LIGHT_LEVEL, 11), 3);
+        String recipeID = recipe.getId().toString();
+        if (recipeID.equals("exspectriments:fusion_shrine/crystal_to_topaz")) {
+            setCrystalInFusionShrine(world, blockPos, fusionShrineBlockEntity, ExspFluids.LIQUID_TOPAZ);
+        } else if (recipeID.equals("exspectriments:fusion_shrine/crystal_to_amethyst")) {
+            setCrystalInFusionShrine(world, blockPos, fusionShrineBlockEntity, ExspFluids.LIQUID_AMETHYST);
+        } else if (recipeID.equals("exspectriments:fusion_shrine/crystal_to_citrine")) {
+            setCrystalInFusionShrine(world, blockPos, fusionShrineBlockEntity, ExspFluids.LIQUID_CITRINE);
+        } else if (recipeID.equals("exspectriments:fusion_shrine/crystal_to_onyx")) {
+            setCrystalInFusionShrine(world, blockPos, fusionShrineBlockEntity, ExspFluids.LIQUID_ONYX);
+        } else if (recipeID.equals("exspectriments:fusion_shrine/crystal_to_moonstone")) {
+            setCrystalInFusionShrine(world, blockPos, fusionShrineBlockEntity, ExspFluids.LIQUID_MOONSTONE);
+        } else if (recipeID.startsWith("exspectriments:fusion_shrine/to_crystal/")) {
+            setCrystalInFusionShrine(world, blockPos, fusionShrineBlockEntity, SpectrumFluids.LIQUID_CRYSTAL);
         }
+    }
+
+    private static void setCrystalInFusionShrine(World world, BlockPos blockPos, FusionShrineBlockEntity fusionShrineBlockEntity, Fluid fluid) {
+        fusionShrineBlockEntity.fluidStorage.variant = FluidVariant.of(fluid);
+        fusionShrineBlockEntity.fluidStorage.amount = BUCKET;
+        world.setBlockState(blockPos, (BlockState)world.getBlockState(blockPos).with(FusionShrineBlock.LIGHT_LEVEL, 11), 3);
     }
 }
